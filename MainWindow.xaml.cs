9562166c6e89562bedcccff66556c13ef95a635f
lanashreet2004-del <lanashreet2004@gmail.com>
2026-05-12 14:48:12 +0300

    first push of the prohect
    open , drop drag , save , restart , RGB channels all Done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Win32;
+using PixalLap.Services;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
+
+
+namespace PixalLap
+{
+    public partial class MainWindow : Window
+    {
+        private BitmapImage originalImage;
+        private BitmapImage currentImage;
+
+        private ImageService imageService = new ImageService();
+
+        private ColorProcessingService colorProcessingService =
+    new ColorProcessingService();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // فتح صورة
+        private void OpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (dialog.ShowDialog() == true)
+            {
+                originalImage = imageService.LoadImage(dialog.FileName);
+
+                currentImage = originalImage;
+
+                MainImage.Source = currentImage;
+            }
+        }
+
+        // حفظ الصورة
+        private void SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentImage != null)
+            {
+                imageService.SaveImage(currentImage);
+            }
+        }
+
+        // إعادة ضبط الصورة
+        private void ResetImage_Click(object sender, RoutedEventArgs e)
+        {
+            if (originalImage != null)
+            {
+                currentImage = originalImage;
+
+                MainImage.Source = currentImage;
+            }
+        }
+
+        // السحب والإفلات
+        private void ImageDropArea_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length > 0)
+                {
+                    originalImage = imageService.LoadImage(files[0]);
+
+                    currentImage = originalImage;
+
+                    MainImage.Source = currentImage;
+                }
+            }
+        }
+
+        // السماح بالسحب
+        private void ImageDropArea_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+
+            e.Handled = true;
+        }
+
+        private void RGBSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (currentImage == null)
+                return;
+
+            double red = RedSlider.Value;
+            double green = GreenSlider.Value;
+            double blue = BlueSlider.Value;
+
+            RedValueText.Text = red.ToString("0.0");
+            GreenValueText.Text = green.ToString("0.0");
+            BlueValueText.Text = blue.ToString("0.0");
+
+            BitmapSource processedImage =
+                colorProcessingService.AdjustRGBChannels(
+                    originalImage,
+                    red,
+                    green,
+                    blue);
+
+            currentImage = ConvertBitmapSourceToBitmapImage(processedImage);
+
+            MainImage.Source = currentImage;
+        }
+
+        private BitmapImage ConvertBitmapSourceToBitmapImage(BitmapSource bitmapSource)
+        {
+            using (System.IO.MemoryStream memory =
+                   new System.IO.MemoryStream())
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+
+                encoder.Frames.Add(
+                    BitmapFrame.Create(bitmapSource));
+
+                encoder.Save(memory);
+
+                memory.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption =
+                    BitmapCacheOption.OnLoad;
+
+                bitmapImage.StreamSource = memory;
+
+                bitmapImage.EndInit();
+
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ Services/ImageService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PixalLap.Services
+{
+    public class ImageService
+    {
+        public BitmapImage LoadImage(string path)
+        {
+            BitmapImage bitmap = new BitmapImage();
+
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(path);
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.EndInit();
+
+            return bitmap;
+        }
+
+        public void SaveImage(BitmapImage image)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg";
+
+            if (dialog.ShowDialog() == true)
+            {
+                BitmapEncoder encoder;
+
+                if (dialog.FilterIndex == 1)
+                {
+                    encoder = new PngBitmapEncoder();
+                }
+                else
+                {
+                    encoder = new JpegBitmapEncoder();
+                }
+
+                encoder.Frames.Add(BitmapFrame.Create(image));
+
+                using (var stream = File.Create(dialog.FileName))
+                {
+                    encoder.Save(stream);
+                }
+            }
+        }
+    }
+}
